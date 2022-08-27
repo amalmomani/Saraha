63233d5d411c66f49c1919c5812b37bade279fa3
amalmomani99@yahoo.com <amalmomani99@yahoo.com>
2022-08-27 14:29:29 +0300

    UserReport

--- Saraha.Core/DTO/UserReport.cs
+++ Saraha.Core/DTO/UserReport.cs
@@ -11,6 +11,10 @@ namespace Saraha.Core.DTO
         public string username { set; get; }
         public string message { set; get; }
         public string ImageName { set; get; }
+        public string email { set; get; }
         public int count { set; get; }
+        public int userId { set; get; }
+        public int loginId { set; get; }
+        public bool is_blocked { set; get; }
     }
 }
